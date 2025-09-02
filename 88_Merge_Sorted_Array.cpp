7686bb0db1f1f3eb77d8ba11f92e9affebcd92eb
Your Name <your.email@example.com>
2025-09-02 23:58:11 +0530

    Modifying the code

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -1,49 +1,46 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    vector<int>ans;
-    int i=0;
-    int j=0;
-    while(i<m && j<n){
-        if(nums1[i]<=nums2[j]){
-            ans.push_back(nums1[i]);
-            i++;
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int idx = m + n - 1;
+    int i = m - 1;
+    int j = n - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] < nums2[j])
+        {
+            nums1[idx] = nums2[j];
+            j--;
+            idx--;
         }
-        else{
-            ans.push_back(nums2[j]);
-            j++;
+        else
+        {
+            nums1[idx] = nums1[i];
+            i--;
+            idx--;
         }
     }
 
-    while (i<m)
+    while (j >= 0)
     {
-        ans.push_back(nums1[i]);
-        i++;
+        nums1[idx] = nums2[j];
+        idx--, j--;
     }
-    while (j<n)
-    {
-        ans.push_back(nums2[j]);
-        j++;
-    }
-    
-    for(int x:ans){
-        cout<<x<<" ";
-    }
-    
 
-    
-        
+    for (int x : nums1)
+    {
+        cout << x << " ";
     }
+}
 int main()
 {
-    vector<int> nums1={1,2,3};
-    vector<int> nums2={2,5,6};
-    int m=nums1.size(),n=nums2.size();    
-    merge(nums1, m, nums2,  n);
-    
+    vector<int> nums1 = {0};
+    vector<int> nums2 = {1};
+    int m = 0, n = 1;
+    merge(nums1, m, nums2, n);
 
-return 0;
-}   
+    return 0;
+}
